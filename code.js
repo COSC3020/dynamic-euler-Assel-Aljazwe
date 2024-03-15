22e170fdd00a2f11db743b8ec30fc9315dd56798
Assel-Aljazwe <157559559+Assel-Aljazwe@users.noreply.github.com>
2024-03-14 20:59:39 -0600

    Update code.js

--- code.js
+++ code.js
@@ -8,7 +8,7 @@ function factorial(n) {
 
 function e(n) {
     let sum = 1.0; // Start with the first term of the series
-    let fact = 1;  // Initialize factorial result for 0!
+    let fact = 1;  // Initializing factorial result for 0!
     for (let i = 1; i <= n; i++) {
         fact *= i;  // Calculating i! iteratively to avoid repeated work
         sum += 1.0 / fact;
